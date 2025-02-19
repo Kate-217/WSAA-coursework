e9eccb655d544d25104cc50d41586b6bbef934b4
Kate Lisovenko <kate.spb@gmail.com>
2025-02-19 21:03:18 +0000

    code to print out the latitudes

--- labs/lab01/trains.py
+++ labs/lab01/trains.py
@@ -15,11 +15,22 @@ with open("trainxml.xml","w") as xmlfp:
  # I.e. find the listings and iterate through them 
  # to print each traincode out. Check it works 
  
-objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
+
+#objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
+#for objTrainPositionsNode in objTrainPositionsNodes:
+#    traincodenode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
+#    traincode = traincodenode.firstChild.nodeValue.strip()
+#    print (traincode)
+
+# 5. Comment out the print and modify the program 
+# so that it prints out the latitudes
+
+objTrainPositionsNodes = doc.getElementsByTagName('objTrainPositions')
 for objTrainPositionsNode in objTrainPositionsNodes:
-    traincodenode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
-    traincode = traincodenode.firstChild.nodeValue.strip()
-    print (traincode)
+     TrainLatitudenodes = objTrainPositionsNode.getElementsByTagName('TrainLatitude').item(0)
+     TrainLatitude = TrainLatitudenodes.firstChild.nodeValue.strip()
+     print(TrainLatitude)
+    
     
     
 
