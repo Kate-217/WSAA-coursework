71b41472c4f21b047b211ec06acef0611dff28b5
Kate Lisovenko <kate.spb@gmail.com>
2025-02-20 19:07:59 +0000

    calculation of pairs and triples deleted

--- assignments/assignment2-carddraw.py
+++ assignments/assignment2-carddraw.py
@@ -1,5 +1,5 @@
 # Author Katerina Lisovenko 
-# This program "deals" (prints out) n cards and calculates duplicates and triples in its values.
+# This program "deals" (prints out) n cards
 
 
 import requests
@@ -9,19 +9,19 @@ from collections import Counter
 # Shuffle and get the deck_id
 
 deck_count = 1 #count for decks
+count = 5 #count for cards
+
 shuffle_url = f"https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count={deck_count}"
 response = requests.get(shuffle_url).json()
 deck_id = response['deck_id']
-
 #print(deck_id)
-# get the cards
 
-count=5 #count for cards
+# get the cards
 draw_url = f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count={count}"
 cards = requests.get(draw_url).json()["cards"]
-# print(cards)
-#print(type(cards))
+#print(cards)
 
+# print the cards
 values = []
 suits = []
 print("Your cards:")
@@ -32,16 +32,7 @@ for card in cards:
 #print(f'Values: {values}')
 #print(f'Suits: {suits}')
 
-# searched with chatGPT:
-counts = Counter(values) 
 
-pairs = len([count for count in counts.values() if count == 2])
-triples = len([count for count in counts.values() if count == 3])
-
-if pairs == 1:
-    print('You have a pair')
-if triples == 1:
-    print('You have a triple')
 
 
 
