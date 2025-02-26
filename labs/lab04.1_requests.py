88a61d2ff25790a805790cddead91e6ae0be7571
Kate Lisovenko <kate.spb@gmail.com>
2025-02-26 15:52:27 +0000

    readbook() with id works

--- labs/lab04.1_requests.py
+++ labs/lab04.1_requests.py
@@ -4,7 +4,7 @@ url = "http://andrewbeatty1.pythonanywhere.com/books"
 #response = requests.get(url)
 #print(response.json())
 
-# Convert that into a function and call it 
+# 3. Convert that into a function and call it 
 # from inside a if __name__ == “__main__”:
 
 def read_books():
@@ -12,7 +12,17 @@ def read_books():
     return response.json()
 
 
+#if __name__ == "__main__":
+    #print(read_books())
+
+# 4. Write the function for find by id and test it.
+
+def read_book(id):
+    geturl = url + "/" + str(id)
+    response = requests.get(geturl)
+    return response.json()
+
 if __name__ == "__main__":
-    print(read_books())
-    
-    
+    print(read_book(1602))
+
+ 
