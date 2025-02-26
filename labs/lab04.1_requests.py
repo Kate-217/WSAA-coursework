e1dc41d324e242ad7e487d45977da492b1b16a94
Kate Lisovenko <kate.spb@gmail.com>
2025-02-26 16:18:57 +0000

    function for updating books done

--- labs/lab04.1_requests.py
+++ labs/lab04.1_requests.py
@@ -31,6 +31,18 @@ def create_book(book):
     response = requests.post(url, json=book)
     return response.json()
 
+#if __name__ == "__main__":
+#    new_book = {"author":"new", "price":99, "title":"new_title"}
+#    print(create_book(new_book)) 
+    
+# update function:
+
+def update_book(id,book):
+    puturl = url + "/" + str(id)
+    response = requests.put(puturl, json=book)
+    return response.json()
+
+
 if __name__ == "__main__":
-    new_book = {"author":"new", "price":99, "title":"new_title"}
-    print(create_book(new_book)) 
+    update = {"author":"A.Dumas","price":88, "title":"The Vicomte de Bragelonne"}
+    print(update_book(1609,update)) 
