7c8d44b1893056f81d9e4acc82fe29699d01634b
Kate Lisovenko <kate.spb@gmail.com>
2025-02-26 16:09:57 +0000

    def create_book() works

--- labs/lab04.1_requests.py
+++ labs/lab04.1_requests.py
@@ -18,11 +18,19 @@ def read_books():
 # 4. Write the function for find by id and test it.
 
 def read_book(id):
-    geturl = url + "/" + str(id)
-    response = requests.get(geturl)
+    get_url = url + "/" + str(id)
+    response = requests.get(get_url)
     return response.json()
 
 if __name__ == "__main__":
     print(read_book(1602))
+    
+# 5.Write the code to create and test it.
 
- 
+def create_book(book):
+    response = requests.post(url, json=book)
+    return response.json()
+
+if __name__ == "__main__":
+    new_book = {"author":"new", "price":99, "title":"new_title"}
+    print(create_book(new_book)) 
