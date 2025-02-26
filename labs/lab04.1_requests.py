ced75da5918d91c3e9102ec47213c1cebe23423f
Kate Lisovenko <kate.spb@gmail.com>
2025-02-26 15:34:42 +0000

    read_books() function added

--- labs/lab04.1_requests.py
+++ labs/lab04.1_requests.py
@@ -1,5 +1,18 @@
 import requests
 
-url = "http://google.com"
-response = requests.get(url)
-print(response.text)
+url = "http://andrewbeatty1.pythonanywhere.com/books"
+#response = requests.get(url)
+#print(response.json())
+
+# Convert that into a function and call it 
+# from inside a if __name__ == “__main__”:
+
+def read_books():
+    response = requests.get(url)
+    return response.json()
+
+
+if __name__ == "__main__":
+    print(read_books())
+    
+    
