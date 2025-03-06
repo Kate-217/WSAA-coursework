a17288da252e6ca578e9e1cd8a98eeca03a8e50f
Kate Lisovenko <kate.spb@gmail.com>
2025-03-06 13:33:38 +0000

    test file for lab05_03_githubbymodule

--- labs/lab05_03_githubbymodule.py
+++ labs/lab05_03_githubbymodule.py
@@ -7,5 +7,10 @@ g = Github(apikey)
 #for repo in g.get_user().get_repos():
 #    print(repo.name)
 
+#getting the clone URL
 repo = g.get_repo("Kate-217/aprivateone")
 print(repo.clone_url)
+
+file_info = repo.get_contents("WSAA-coursework/labs/test.txt")
+url_file = file_info.download_url
+print (url_file)
