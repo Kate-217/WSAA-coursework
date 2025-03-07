5bb74b28e5eb338e718601fef23f4924a2b84e31
Kate Lisovenko <kate.spb@gmail.com>
2025-03-07 10:23:21 +0000

    updated a file on git by prog

--- labs/lab05_03_githubbymodule.py
+++ labs/lab05_03_githubbymodule.py
@@ -24,3 +24,11 @@ print(f"This is in the file: {file_content}")
 new_content = file_content + " I have just added this line via VS Code \n"
 print (new_content)
 
+#updating file on githab
+new_content_2 = "This is a new content"
+gitHubResponse=repo.update_file(
+    file_info.path, # path 
+    "updated by prog", # commit message
+    new_content_2, # new content
+    file_info.sha) 
+print (gitHubResponse)
