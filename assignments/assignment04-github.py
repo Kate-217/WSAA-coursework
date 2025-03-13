374504d0617b3764aa9fa77c574734dc2f68e5cc
Kate Lisovenko <kate.spb@gmail.com>
2025-03-13 12:43:12 +0000

    venv and requirements created

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -1,3 +1,6 @@
+# Assignment 04
+# Author Katerina Lisovenko
+
 from github import Github
 import config1
 import requests
@@ -5,16 +8,14 @@ import requests
 apikey = config1.git_key
 g = Github(apikey)
 
-# Cheking out the repo
 for repo in g.get_user().get_repos():
     print(repo.name)
     
 file_path = "assignments/story.txt"    
 
-#getting the clone URL
+#getting the URL
 repo = g.get_repo("Kate-217/WSAA-coursework")
 print(repo.clone_url) 
-
 
 # getting the file url
 file_info = repo.get_contents(file_path)
@@ -25,15 +26,16 @@ response = requests.get(url_file)
 content = response.text
 print("Content of the file:", content)
 
-
-new_content = content.replace("Andrew", "Kate")    
+#updating
+new_content = content.replace("Andrew","Kate")    
 
 #updating file on githab
 gitHubResponse=repo.update_file(
     file_path, # path 
-    "updated by prog", # commit message
+    "Works well, the final vertion of replacement", # commit message
     new_content, # new content
     file_info.sha) 
 print (gitHubResponse)
 
+# Use this if you need to get the initial vertion back:
 #repo.update_file(file_path, "Reverted to original version", content, file_info.sha)
